98abc84fa68939ec5633ed751f16dff62275634d
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-16 21:13:05 +0530

    adding new ds anagrams problem

--- All DS Question-Answer.java
+++ All DS Question-Answer.java
@@ -1138,3 +1138,75 @@ class Solution {
     return -1;
     }
 }
+--------------------------------------------------------------------------
+LeetCode 3. Longest Substring Without Repeating Characters
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+ class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map= new HashMap<Character, Integer>();
+        int end =0;
+        int result =0;
+        for(int i=0; i< s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                end = Math.max(end, map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i), i);
+            result=Math.max(result, i-end+1);
+        }
+        return result;
+    }
+}
+--------------------------------------------------------------------------
+LeetCode 49. Group Anagrams
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            map.putIfAbsent(sortedWord, new ArrayList<>());
+            map.get(sortedWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
