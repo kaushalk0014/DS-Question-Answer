fd11efda9cad1e53220a704d14a0aec40ceeff0f
kaushal kumar <kaushal.k.0014@gmail.com>
2025-03-18 11:16:06 +0530

    Remove Duplicates from Sorted Array

--- All DS Question-Answer.java
+++ All DS Question-Answer.java
@@ -1210,3 +1210,34 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+--------------------------------------------------------------------------
+Leetcode 26. Remove Duplicates from Sorted Array
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums!=null & nums.length==0){
+            return 0;
+        }
+        int k=0;
+        for(int i=1; i< nums.length; i++){
+            if(nums[i] != nums[k]){
+                k++;
+                nums[k]=nums[i];
+            }
+        }
+        return k+1;
+    }
+}
